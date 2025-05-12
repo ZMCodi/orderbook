1071dfb6e1aaf6f3eeef80835e0d0399d57fb612
ZMCodi Mac <zaim.mohsin@gmail.com>
2025-05-12 20:17:24 +0100

    basic skeleton

--- main.cpp
+++ main.cpp
@@ -1,14 +1,60 @@
 // for catch testing
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch_all.hpp>
+// #define CATCH_CONFIG_MAIN
+// #include <catch2/catch_all.hpp>
 
 #include <iostream>
-
-int test() {return 3;}
-
-TEST_CASE("OrderBook")
+#include <array>
+#include <string>
+class Order
 {
-    REQUIRE(test() == 3);
-    REQUIRE_FALSE(test() == 5);
-}
+public:
+    enum Side
+    {
+        BUY,
+        SELL
+    };
+    static constexpr std::array<std::string, 2> side_str{"BUY", "SELL"};
 
+    enum Type
+    {
+        LIMIT,
+        MARKET
+    };
+    static constexpr std::array<std::string, 2> type_str{"LIMIT", "MARKET"};
+
+    Order(Side side, int volume, Type type, float price)
+    : side{side}
+    , volume{volume}
+    , type{type}
+    , price{price} {}
+
+    friend std::ostream& operator<<(std::ostream& out, Order order)
+    {
+        return out << "Order(SIDE:" << Order::side_str[order.side] << ", VOL:" << order.volume
+            << ", TYPE:" << Order::type_str[order.type] << ", PRICE:" << order.price << ")";
+    }
+
+private:
+    Side side;
+    int volume;
+    Type type;
+    float price;
+
+};
+
+class Trade
+{
+
+};
+
+class OrderBook
+{
+
+};
+
+int main()
+{
+    Order order{Order::BUY, 3, Order::MARKET, 20};
+    Order order2{Order::SELL, 5, Order::LIMIT, 50};
+    std::cout << order << '\n' << order2 << std::endl;
+}
