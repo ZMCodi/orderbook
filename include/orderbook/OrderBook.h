c3410aa15d0f47fc7c2d230ea2eb1e67a3c8a92e
ZMCodi Mac <zaim.mohsin@gmail.com>
2025-05-14 02:33:23 +0100

    add test helper to check orderbook state

--- include/orderbook/OrderBook.h
+++ include/orderbook/OrderBook.h
@@ -7,32 +7,46 @@
 #include <list>
 #include <map>
 
+// linked list of orders, easy to swap out implementation by just changing it here
+using order_list = std::list<Order>;
+
+// aggregrates the volume at a given price level so no need to recalculate
+// when getting depth
+struct PriceLevel
+{
+    int volume{};
+    order_list orders;
+};
+
+// bid/ask map = {price: (volume, [orders...])}
+using bid_map = std::map<float, PriceLevel, std::greater<float>>;
+using ask_map = std::map<float, PriceLevel>;
+
+struct OrderLocation
+{
+    float price;
+    order_list::iterator itr; // O(1) access to cancel/modify/fetch
+    Order::Side side; // easier to determine which map to search
+};
+
+// id map = {id: (price, &order, BUY/SELL)}
+using id_map = std::unordered_map<std::string, OrderLocation>;
+
+// stores state for testing
+struct OrderBookState
+{
+    bid_map bidMap{};
+    ask_map askMap{};
+    id_map idMap{};
+
+    float bestBid{-1};
+    float bestAsk{-1};
+    float marketPrice{-1};
+    int totalVolume{};
+};
+
 class OrderBook
 {
-    // linked list of orders, easy to swap out implementation by just changing it here
-    using order_list = std::list<Order>;
-
-    // aggregrates the volume at a given price level so no need to recalculate
-    // when getting depth
-    struct PriceLevel
-    {
-        int volume{};
-        order_list orders;
-    };
-
-    // bid/ask map = {price: (volume, [orders...])}
-    using bid_map = std::map<float, PriceLevel, std::greater<float>>;
-    using ask_map = std::map<float, PriceLevel>;
-
-    struct OrderLocation
-    {
-        float price;
-        order_list::iterator itr; // O(1) access to cancel/modify/fetch
-        Order::Side side; // easier to determine which map to search
-    };
-
-    // id map = {id: (price, &order, BUY/SELL)}
-    using id_map = std::unordered_map<std::string, OrderLocation>;
 public:
 
     // snapshot at each price level
@@ -73,13 +87,14 @@ public:
     float getMarketPrice() {return marketPrice;}
     int getTotalVolume() {return totalVolume;}
     float getSpread() {return bestAsk - bestBid;}
+    friend bool checkOBState(const OrderBook& ob, const OrderBookState& state); // for testing
 
     std::list<Order> dummy{{Order::Side::BUY, 3, Order::Type::LIMIT, 50}};
 
 private:
     bid_map bidMap{};
     ask_map askMap{};
-    id_map orderIDs{};
+    id_map idMap{};
 
     float bestBid{-1};
     float bestAsk{-1};
--- tests/test_helpers.h
+++ tests/test_helpers.h
@@ -2,6 +2,8 @@
 
 #include <chrono>
 #include <list>
+#include <exception>
+#include <iostream>
 #include "orderbook/OrderBook.h"
 
 // helper for timestamps
@@ -21,6 +23,66 @@ inline bool compareOrderLists(const std::list<Order>& first, const std::list<Ord
         if (!(*f).equals_to(*s)) {return false;}
         ++f;
         ++s;
+    }
+
+    return true;
+}
+
+inline bool checkOBState(const OrderBook& ob, const OrderBookState& state)
+{
+    // compare simple states
+    if (ob.bestBid != state.bestBid) {return false;}
+    if (ob.bestAsk != state.bestAsk) {return false;}
+    if (ob.marketPrice != state.marketPrice) {return false;}
+    if (ob.totalVolume != state.totalVolume) {return false;}
+
+    try
+    {
+        // compare bid map
+        if (ob.bidMap.size() != state.bidMap.size()) {return false;}
+        for (const auto& [price, expLevel] : state.bidMap)
+        {
+            const auto& [expVolume, expOrders] = expLevel;
+            const auto& [actVolume, actOrders] = ob.bidMap.at(price);
+
+            if (expVolume != actVolume) {return false;}
+
+            if (expOrders.size() != actOrders.size()) {return false;}
+
+            if (!compareOrderLists(expOrders, actOrders)) {return false;}
+        }
+
+        // compare ask map
+        if (ob.askMap.size() != state.askMap.size()) {return false;}
+        for (const auto& [price, expLevel] : state.askMap)
+        {
+            const auto& [expVolume, expOrders] = expLevel;
+            const auto& [actVolume, actOrders] = ob.askMap.at(price);
+
+            if (expVolume != actVolume) {return false;}
+
+            if (expOrders.size() != actOrders.size()) {return false;}
+
+            if (!compareOrderLists(expOrders, actOrders)) {return false;}
+        }
+
+        // compare id map
+        if (ob.idMap.size() != state.idMap.size()) {return false;}
+        for (const auto& [id, expLocation] : state.idMap)
+        {
+            const auto& [expPrice, expItr, expSide] = expLocation;
+            const auto& [actPrice, actItr, actSide] = ob.idMap.at(id);
+
+            if (expPrice != actPrice) {return false;}
+            if (!expItr->equals_to(*actItr)) {return false;}
+            if (expSide != actSide) {return false;}
+        }
+
+    } catch (const std::exception& e) 
+    {
+        // put this here first to test the method actually works
+        std::cout << "Error: " << e.what();
+        return false; // .at() throws an error
     }
 
     return true;
