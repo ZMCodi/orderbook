82a21724af725aff09b9c26e86beb93b11dd5ac3
ZMCodi Mac <zaim.mohsin@gmail.com>
2025-05-15 11:01:50 +0100

    make trade fields const

--- include/orderbook/Trade.h
+++ include/orderbook/Trade.h
@@ -5,15 +5,15 @@
 struct Trade
 {
 public:
-    const uuids::uuid* id;
-    const uuids::uuid* buyer_id;
-    const uuids::uuid* seller_id;
-    float price;
-    int volume;
-    std::chrono::time_point<std::chrono::system_clock> timestamp;
-    Order::Side taker;
+    const uuids::uuid* const id;
+    const uuids::uuid* const buyer_id;
+    const uuids::uuid* const seller_id;
+    const float price;
+    const int volume;
+    const std::chrono::time_point<std::chrono::system_clock> timestamp;
+    const Order::Side taker;
 
     bool equals_to(const Trade& other) const; // for testing
     bool operator==(const Trade& other) const;
-    const uuids::uuid* get_id();
+    const uuids::uuid* get_id() const;
 };
